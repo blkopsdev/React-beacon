9ee79a21be36609229e138fdf3ae73df9157f16a
Jonathan Flower <jon@simplejoysolutions.com>
2018-05-31 15:10:39 -0500

    testing pretty 7

--- src/actions/userActions.tsx
+++ src/actions/userActions.tsx
@@ -1,41 +1,43 @@
-import axios from "axios"
-import * as types from "./actionTypes"
-import API from "../constants/apiEndpoints"
-import { beginAjaxCall } from "./ajaxStatusActions"
-import { toastr } from "react-redux-toastr"
+import axios from 'axios';
+import * as types from './actionTypes'
+import API from '../constants/apiEndpoints'
+import { beginAjaxCall } from './ajaxStatusActions'
+import { toastr } from 'react-redux-toastr'
 
 // import {BasicToastrOptions} from 'react-redux-toastr/index.d'
 // import {Iuser} from '../models';
 
 export function userLogin(
   event: any,
-  email: string = "test",
-  password: string = "test"
+  email: string = 'test',
+  password: string = 'test'
 ) {
   return (dispatch: any, getState: any) => {
-    dispatch(beginAjaxCall())
+    dispatch(beginAjaxCall());
     return axios
       .get(API.GET.user.login, { params: { email, password } })
       .then(data => {
         if (!data.data) {
           throw undefined;
         } else {
-          dispatch({ type: types.USER_LOGIN_SUCCESS, user: data.data })
-          toastr.success("Success", "Logged in.")
+          dispatch({ type: types.USER_LOGIN_SUCCESS, user: data.data });
+          toastr.success('Success', 'Logged in.');
           return data;
         }
       })
       .catch((error: any) => {
-        dispatch({ type: types.USER_LOGIN_FAILED })
-        let msg = error.message || "Failed to login.  Please try again or contact support."
+        dispatch({ type: types.USER_LOGIN_FAILED });
+        let msg =
+          error.message ||
+          'Failed to login.  Please try again or contact support.';
         if (!navigator.onLine) {
-          msg = "Please connect to the internet.";
+          msg = 'Please connect to the internet.';
         }
-        toastr.error("Error", msg, {
-          transitionIn: "bounceInDown",
-          transitionOut: "bounceOutUp",
+        toastr.error('Error', msg, {
+          transitionIn: 'bounceInDown',
+          transitionOut: 'bounceOutUp',
           timeOut: 0
-      });
+        });
         throw error;
       });
   };

42e9da3ae76d8e6bd5bd20a412cdaf499d95ddbe
Jonathan Flower <jon@simplejoysolutions.com>
2018-06-07 09:17:58 -0500

    Header split to HeaderMenu

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,60 +1,15 @@
 import * as React from 'react';
-import { connect } from 'react-redux';
-import { InitialState, Iuser } from '../../models';
-import { userLogout } from '../../actions/userActions';
-import { isAuthenticated } from '../../constants/adalConfig';
-import FontAwesomeIcon from '@fortawesome/react-fontawesome';
-import * as faCog from '@fortawesome/fontawesome-free-solid/faCog';
+import HeaderMenu from './HeaderMenu';
 
 const headerImg = require('src/images/KittenLogo@2x.png');
 
-interface Iprops extends React.Props<Header> {
-  userLogout?: any;
-}
-interface Istate {
-  user: Iuser;
-}
-
-class Header extends React.Component<Iprops, Istate> {
-  constructor(props: Iprops) {
-    super(props);
-  }
-
-  getProfile() {
-    if (isAuthenticated()) {
-      return (
-        <span className="profile">
-          WELCOME&nbsp;
-          <span className="name"> Fluffy</span>
-          <span className="vertical" />
-          <FontAwesomeIcon
-            icon={faCog}
-            size="lg"
-            onClick={this.props.userLogout}
-          />
-        </span>
-      );
-    }
-    return '';
-  }
-
-  render() {
-    return (
-      <div className="header">
-        <img src={headerImg} />
-        {this.getProfile()}
-      </div>
-    );
-  }
-}
-
-const mapStateToProps = (state: InitialState, ownProps: Iprops) => {
-  return {
-    user: state.user
-  };
+const Header = () => {
+  return (
+    <div className="header">
+      <img src={headerImg} />
+      <HeaderMenu />
+    </div>
+  );
 };
 
-export default connect(
-  mapStateToProps,
-  { userLogout }
-)(Header);
+export default Header;
---
+++ src/components/header/HeaderMenu.tsx
@@ -0,0 +1,54 @@
+/*
+* The HeaderMenu only displays if we have an authenticated user.
+*/
+
+import * as React from 'react';
+import { connect } from 'react-redux';
+import { InitialState, Iuser } from '../../models';
+import { userLogout } from '../../actions/userActions';
+import { isAuthenticated } from '../../constants/adalConfig';
+import FontAwesomeIcon from '@fortawesome/react-fontawesome';
+import * as faCog from '@fortawesome/fontawesome-free-solid/faCog';
+
+interface Iprops extends React.Props<Header> {
+  userLogout?: any;
+}
+interface Istate {
+  user: Iuser;
+}
+
+class Header extends React.Component<Iprops, Istate> {
+  constructor(props: Iprops) {
+    super(props);
+  }
+
+  render() {
+    if (isAuthenticated()) {
+      return (
+        <span className="profile">
+          WELCOME&nbsp;
+          <span className="name"> Fluffy</span>
+          <span className="vertical" />
+          <FontAwesomeIcon
+            icon={faCog}
+            size="lg"
+            onClick={this.props.userLogout}
+          />
+        </span>
+      );
+    } else {
+      return null;
+    }
+  }
+}
+
+const mapStateToProps = (state: InitialState, ownProps: Iprops) => {
+  return {
+    user: state.user
+  };
+};
+
+export default connect(
+  mapStateToProps,
+  { userLogout }
+)(Header);

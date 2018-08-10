b6cf12dec60cce081405ca8660cbe704538fde4f
Jonathan Flower <jon@simplejoysolutions.com>
2018-08-10 09:14:54 -0500

    removing null from userqueue

--- src/reducers/userQueueReducer.tsx
+++ src/reducers/userQueueReducer.tsx
@@ -1,15 +1,18 @@
 import * as types from '../actions/actionTypes';
 import { IqueueUser } from '../models';
 // import initialState from './initialState';
-import { filter } from 'lodash';
+import { pickBy, map, filter } from 'lodash';
 
 export default function userQueue(state: IqueueUser[] = [], action: any) {
   switch (action.type) {
     case types.USER_QUEUE_SUCCESS:
-      // return map(action.queue, (queueObject) => {
-      //   return {...queueObject, user : filter(queueObject.user, (property, index, key) => property !== null)}
-      // })
-      return [...action.queue];
+      return map(action.queue, queueObject => {
+        return {
+          ...queueObject,
+          user: pickBy(queueObject.user, (property, key) => property !== null)
+        };
+      });
+    // return [...action.queue];
     // return [...state, ...action.queue];
     // return Object.assign(
     //   {},

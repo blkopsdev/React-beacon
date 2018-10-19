ed56e9e379c05bb01c4d1f401f020d0cc8c5f70c
Jonathan Flower <jon@simplejoysolutions.com>
2018-10-19 14:12:23 -0500

    added location api endpoints

--- src/constants/apiEndpoints.tsx
+++ src/constants/apiEndpoints.tsx
@@ -3,6 +3,31 @@ console.log(`ENV: ${process.env.NODE_ENV} SERVER_DOMAIN: ${root}`);
 
 const API = {
   POST: {
+    building: `${root}building`,
+    customer: {
+      add: `${root}customer/add`
+    },
+
+    job: {
+      create: `${root}job/create`
+    },
+
+    facility: {
+      add: `${root}facility/add`
+    },
+    floor: `${root}floor`,
+    location: `${root}location`,
+    room: `${root}room`,
+    inventory: {
+      updateproduct: `${root}inventory/updateproduct`,
+      addproduct: `${root}inventory/addproduct`,
+      quote: `${root}inventory/quote`,
+      updateinstall: `${root}inventory/update`,
+      addinstall: `${root}inventory/add`,
+      deleteInstall: `${root}inventory/delete`,
+      installContact: `${root}inventory/contact`,
+      approveproduct: `${root}inventory/approve`
+    },
     user: {
       login: `${root}user/login`,
       signup: `${root}user/signup`,
@@ -13,25 +38,6 @@ const API = {
       updateteam: `${root}user/updateteammember`,
       saveteam: `${root}user/saveteammember`,
       deleteTeamMember: `${root}user/deleteteammember`
-    },
-    job: {
-      create: `${root}job/create`
-    },
-    customer: {
-      add: `${root}customer/add`
-    },
-    facility: {
-      add: `${root}facility/add`
-    },
-    inventory: {
-      updateproduct: `${root}inventory/updateproduct`,
-      addproduct: `${root}inventory/addproduct`,
-      quote: `${root}inventory/quote`,
-      updateinstall: `${root}inventory/update`,
-      addinstall: `${root}inventory/add`,
-      deleteInstall: `${root}inventory/delete`,
-      installContact: `${root}inventory/contact`,
-      approveproduct: `${root}inventory/approve`
     }
   },
   PUT: {
@@ -40,12 +46,10 @@ const API = {
     }
   },
   GET: {
-    user: {
-      getuserqueue: `${root}user/getuserqueue`,
-      getfseusers: `${root}user/getfseusers`,
-      getusersearch: `${root}user/search`,
-      getteamsearch: `${root}user/searchteam`
+    building: {
+      getall: `${root}building/getall`
     },
+
     customer: {
       getall: `${root}customer/getall`
     },
@@ -64,6 +68,21 @@ const API = {
       getproductinfo: `${root}inventory/getproductinfo`,
       getproductqueue: `${root}inventory/getproductqueue`,
       products: `${root}inventory/products`
+    },
+    floor: {
+      getall: `${root}floor/getall`
+    },
+    location: {
+      getall: `${root}location/getall`
+    },
+    room: {
+      getall: `${root}room/getall`
+    },
+    user: {
+      getuserqueue: `${root}user/getuserqueue`,
+      getfseusers: `${root}user/getfseusers`,
+      getusersearch: `${root}user/search`,
+      getteamsearch: `${root}user/searchteam`
     }
   }
 };

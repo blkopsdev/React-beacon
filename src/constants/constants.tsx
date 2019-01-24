83eb9f7b65fac42ef322d944bc002539adb0f5ba
Jonathan Flower <jon@simplejoysolutions.com>
2019-01-24 15:45:14 -0600

    moved adal config vars to env file

--- src/constants/constants.tsx
+++ src/constants/constants.tsx
@@ -410,10 +410,6 @@ const tiles = [
 ];
 
 const constants = {
-  adalAuth: {
-    tenant: 'a675e2fc-4806-4ec9-b49c-b0dc413b0e6b',
-    clientId: 'e5fb8173-e048-4cda-8acd-a8e735b4c927'
-  },
   toastrError: {
     transitionIn: 'bounceInDown' as transitionInType,
     transitionOut: 'bounceOutUp' as transitionOutType,

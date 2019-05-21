20599c98b04171211641513ad1f0bd2bbe43851c
phoenix377 <phoenix.number377@gmail.com>
2019-05-21 08:13:49 +0800

    revert async/await to promise

--- src/actions/manageAlertActions.tsx
+++ src/actions/manageAlertActions.tsx
@@ -6,119 +6,140 @@ import { authContext } from './userActions';
 import * as types from './actionTypes';
 import { constants } from '../constants/constants';
 import { toastr } from 'react-redux-toastr';
-import { ItableFiltersParams, IAlert } from '../models';
+import { ItableFiltersParams, IAlert, IinitialState } from '../models';
+import { ThunkAction } from 'redux-thunk';
 
-export const getAlerts = () => async (dispatch: any, getState: any) => {
-  dispatch(beginAjaxCall());
-  const { page, title } = getState().manageAlert.tableFilters;
-  const axiosOptions: AxiosRequestConfig = {
-    method: 'get',
-    params: { page, title }
+type ThunkResult<R> = ThunkAction<R, IinitialState, undefined, any>;
+
+export const getAlerts = () => (): ThunkResult<void> => {
+  return async (dispatch, getState) => {
+    dispatch(beginAjaxCall());
+    const { page, title } = getState().manageAlert.tableFilters;
+    const axiosOptions: AxiosRequestConfig = {
+      method: 'get',
+      params: { page, title }
+    };
+
+    const resource = `${process.env.REACT_APP_ADAL_CLIENTID}`;
+    const url = API.GET.alert.search;
+
+    try {
+      const data: AxiosResponse<any> = await adalFetch(
+        authContext,
+        resource,
+        axios,
+        url,
+        axiosOptions
+      );
+
+      dispatch({ type: types.LOAD_ALERTS_SUCCESS, payload: data.data.result });
+      dispatch({
+        type: types.ALERT_MANAGE_TOTAL_PAGES,
+        pages: data.data.pages
+      });
+    } catch (error) {
+      dispatch({ type: types.LOAD_ALERTS_SUCCESS });
+      constants.handleError(error, 'get alerts');
+    }
   };
-
-  const resource = `${process.env.REACT_APP_ADAL_CLIENTID}`;
-  const url = API.GET.alert.search;
-
-  try {
-    const data: AxiosResponse<any> = await adalFetch(
-      authContext,
-      resource,
-      axios,
-      url,
-      axiosOptions
-    );
-
-    dispatch({ type: types.LOAD_ALERTS_SUCCESS, payload: data.data.result });
-    dispatch({ type: types.ALERT_MANAGE_TOTAL_PAGES, pages: data.data.pages });
-  } catch (error) {
-    dispatch({ type: types.LOAD_ALERTS_SUCCESS });
-    constants.handleError(error, 'get alerts');
-  }
 };
 
-export const saveAlert = (alert: any) => async (dispatch: any) => {
-  dispatch(beginAjaxCall());
-  let headers = {};
-  if (alert instanceof FormData) {
-    headers = { 'content-type': 'multipart/form-data' };
-  }
-  const axiosOptions: AxiosRequestConfig = {
-    method: 'post',
-    data: alert,
-    headers
+export const saveAlert = (alert: IAlert | FormData) => (): ThunkResult<
+  void
+> => {
+  return async dispatch => {
+    dispatch(beginAjaxCall());
+    let headers = {};
+    if (alert instanceof FormData) {
+      headers = { 'content-type': 'multipart/form-data' };
+    }
+    const axiosOptions: AxiosRequestConfig = {
+      method: 'post',
+      data: alert,
+      headers
+    };
+    const resource = `${process.env.REACT_APP_ADAL_CLIENTID}`;
+    const url = API.POST.alert.create;
+
+    try {
+      const data: AxiosResponse<any> = await adalFetch(
+        authContext,
+        resource,
+        axios,
+        url,
+        axiosOptions
+      );
+
+      const newAlert = { ...alert, ...data.data };
+      dispatch({
+        type: types.ADD_ALERT_SUCCESS,
+        payload: newAlert
+      });
+
+      toastr.success('Success', `Created Alert.`, constants.toastrSuccess);
+    } catch (error) {
+      dispatch({ type: types.ADD_ALERT_FAILED });
+      constants.handleError(error, 'ADD_ALERT_FAILED');
+    }
   };
-  const resource = `${process.env.REACT_APP_ADAL_CLIENTID}`;
-  const url = API.POST.alert.create;
-
-  try {
-    const data: AxiosResponse<any> = await adalFetch(
-      authContext,
-      resource,
-      axios,
-      url,
-      axiosOptions
-    );
-
-    const newAlert = { ...alert, ...data.data };
-    dispatch({
-      type: types.ADD_ALERT_SUCCESS,
-      payload: newAlert
-    });
-
-    toastr.success('Success', `Created Alert.`, constants.toastrSuccess);
-  } catch (error) {
-    dispatch({ type: types.ADD_ALERT_FAILED });
-    constants.handleError(error, 'ADD_ALERT_FAILED');
-  }
 };
 
-export const updateAlert = (alert: FormData) => async (dispatch: any) => {
-  dispatch(beginAjaxCall());
-  let headers = {};
-  if (alert instanceof FormData) {
-    headers = { 'content-type': 'multipart/form-data' };
-  }
-  const axiosOptions: AxiosRequestConfig = {
-    method: 'put',
-    data: alert,
-    headers
-  };
-  const resource = `${process.env.REACT_APP_ADAL_CLIENTID}`;
-  const id = alert.get('id') as string;
-  const url = API.PUT.alert.update.replace('{alertId}', id);
+export const updateAlert = (alert: IAlert | FormData) => (): ThunkResult<
+  void
+> => {
+  return async dispatch => {
+    dispatch(beginAjaxCall());
+    let headers = {};
+    let id: string = '';
+    if (alert instanceof FormData) {
+      headers = { 'content-type': 'multipart/form-data' };
+      id = alert.get('id') as string;
+    } else {
+      id = alert.id;
+    }
+    const axiosOptions: AxiosRequestConfig = {
+      method: 'put',
+      data: alert,
+      headers
+    };
+    const resource = `${process.env.REACT_APP_ADAL_CLIENTID}`;
+    const url = API.PUT.alert.update.replace('{alertId}', id);
 
-  try {
-    const data: AxiosResponse<any> = await adalFetch(
-      authContext,
-      resource,
-      axios,
-      url,
-      axiosOptions
-    );
-    dispatch({ type: types.EDIT_ALERT_SUCCESS, payload: data.data });
-    toastr.success('Success', `Updated Alert.`, constants.toastrSuccess);
-  } catch (error) {
-    dispatch({ type: types.EDIT_ALERT_FAILED });
-    constants.handleError(error, 'EDIT_ALERT_FAILED');
-  }
+    try {
+      const data: AxiosResponse<any> = await adalFetch(
+        authContext,
+        resource,
+        axios,
+        url,
+        axiosOptions
+      );
+      dispatch({ type: types.EDIT_ALERT_SUCCESS, payload: data.data });
+      toastr.success('Success', `Updated Alert.`, constants.toastrSuccess);
+    } catch (error) {
+      dispatch({ type: types.EDIT_ALERT_FAILED });
+      constants.handleError(error, 'EDIT_ALERT_FAILED');
+    }
+  };
 };
 
-export const deleteAlert = (alert: IAlert) => async (dispatch: any) => {
-  dispatch(beginAjaxCall());
-  const axiosOptions: AxiosRequestConfig = {
-    method: 'delete'
-  };
-  const resource = `${process.env.REACT_APP_ADAL_CLIENTID}`;
-  const url = API.DELETE.alert.delete.replace('{alertId}', alert.id);
+export const deleteAlert = (alert: IAlert) => (): ThunkResult<void> => {
+  return async dispatch => {
+    dispatch(beginAjaxCall());
+    const axiosOptions: AxiosRequestConfig = {
+      method: 'delete'
+    };
+    const resource = `${process.env.REACT_APP_ADAL_CLIENTID}`;
+    const url = API.DELETE.alert.delete.replace('{alertId}', alert.id);
 
-  try {
-    await adalFetch(authContext, resource, axios, url, axiosOptions);
-    dispatch({ type: types.REMOVE_ALERT_SUCCESS, payload: alert });
-    toastr.success('Success', `Deleted Alert.`, constants.toastrSuccess);
-  } catch (error) {
-    dispatch({ type: types.REMOVE_ALERT_FAILED });
-    constants.handleError(error, 'REMOVE_ALERT_FAILED');
-  }
+    try {
+      await adalFetch(authContext, resource, axios, url, axiosOptions);
+      dispatch({ type: types.REMOVE_ALERT_SUCCESS, payload: alert });
+      toastr.success('Success', `Deleted Alert.`, constants.toastrSuccess);
+    } catch (error) {
+      dispatch({ type: types.REMOVE_ALERT_FAILED });
+      constants.handleError(error, 'REMOVE_ALERT_FAILED');
+    }
+  };
 };
 
 export const setSelectedAlertID = (id: string) => ({
